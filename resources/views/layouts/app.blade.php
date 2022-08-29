4c261b731d558abc1ee6aea06444019088620045
cibs84 <d.tilesi@gmail.com>
2022-08-29 18:00:59 +0200

    created layout and basic pages

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title_page')</title>
+</head>
+<body>
+
+    <header>
+        <nav>
+            <ul>
+                <li><a href="{{ route('homepage') }}">Homepage</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <main>
+        @yield('main_content')
+    </main>
+</body>
+</html>
