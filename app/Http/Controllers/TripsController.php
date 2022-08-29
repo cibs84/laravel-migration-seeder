16b5762eaf7889fc94fc7290def94c576148c194
cibs84 <d.tilesi@gmail.com>
2022-08-29 19:41:35 +0200

    Trip model done

--- app/Http/Controllers/TripsController.php
+++ app/Http/Controllers/TripsController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Trip;
 
 class TripsController extends Controller
 {
     function trips($place = 'I\'m a place var') {
-        dd($place);
-        return view('trips');
+        $trips = Trip::all();
+        dd($trips);
+        return view('trips', $trips);
     }
 }
